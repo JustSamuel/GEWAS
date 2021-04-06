c3ca778bdc2ac7a1641730df94e7677ce862e466
Samuel <samuelincognito@gmail.com>
2021-04-06 13:15:52 +0200

    Added comments.

--- backend/webpack.config.ts
+++ backend/webpack.config.ts
@@ -4,22 +4,36 @@ import path from 'path';
 import webpack from 'webpack';
 import nodeExternals from 'webpack-node-externals'
 
+/**
+ * Basic Webpack configuration.
+ */
 const config: webpack.Configuration = {
+  // We only use webpack in production mode.
   mode: "production",
+
+  // Main build entry point.
   entry: './main.ts',
+
+  // Typescript loader for webpack.
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        // exclude: /node_modules/,
+        test: /\.tsx?$/,         // Target al .ts files
+        use: 'ts-loader',        // Use the ts-loader
+        exclude: /node_modules/, // Ignore the node_modules
       },
     ],
   },
+
+  // Ignore node externals when compiling.
   externals: [ nodeExternals() ],
+
+  // Syntactic sugar
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ],
   },
+
+  // Output everything in a app.js file under dist.
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
