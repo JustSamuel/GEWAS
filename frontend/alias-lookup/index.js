e6675fb5146d150947e64192ad6bb57f153227be
Samuel <samuelincognito@gmail.com>
2025-07-07 01:10:09 +0200

    fix: left testing mistake in

--- frontend/alias-lookup/index.js
+++ frontend/alias-lookup/index.js
@@ -16,7 +16,7 @@ function lookup (email) {
   }
 
   // Make request
-  xmlHttp.open('GET', `http://localhost:9404/api/v1/email/${email}/aliases`, true)
+  xmlHttp.open('GET', `https://mail-api.gewis.nl/api/v1/email/${email}/aliases`, true)
   xmlHttp.send(null)
 
   return false
